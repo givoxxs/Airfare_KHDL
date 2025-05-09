cff7b58ee663982baedb2be4cdc9110f002ebc69
unknown <ntkl1710@gmail.com>
2025-05-09 16:28:35 +0700

    Remove unwanted file

--- model/train_test_split.py
+++
@@ -1,8 +0,0 @@
-from sklearn.model_selection import train_test_split
-import pandas as pd
-df = pd.read_csv('../data/air_fare_raw.csv')
-trainset, testset = train_test_split(df, test_size=0.2, random_state=42)
-trainset.to_csv('../data/train.csv')
-trainset.to_csv('../data/test.csv')
-trainset.reset_index(inplace=True, drop=True)
-testset.reset_index(inplace=True, drop=True)
